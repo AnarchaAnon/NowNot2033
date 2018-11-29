5a8ec8f3ef373751de68e1424669600e219bf59c
AnarchaAnon <43900091+AnarchaAnon@users.noreply.github.com>
2018-11-28 21:51:41 -0500

    Update #NowNot2033.js

--- #NowNot2033.js
+++ #NowNot2033.js
@@ -1,20 +1,40 @@
 var config = require('./config');
 var T = new Twit(config);
 
+/*
+//  this is the command
+//  to post to twitter
+//  and calls functions
+//  ☆~~(>w0✿)b
+*/
 
-//
-//  tweet 'hello world!'
-//
-T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
-  console.log(data)
-})
+T.post('statuses/update', { status: msg }, function(err, data, response) {
+  console.log(data)})
 
 
 /* 
-// js function which
-// counts down how
-// many days are left!
-// ☆~~(>w0✿)b
+//  these variables
+//  are for twitter
+//  to tweet!
+//  ☆~~(>w0✿)b
+*/
+
+function msg() {
+var cntDwn = cntDwn();
+  
+var msg = 'We have ' + cntDwn() + ' day' + sss + ' left to fix climate change!' + tag;
+
+var tag = ' 12 years goes by faster than you think! #NowNot2030'
+
+var fin = 'Well, dang... '
+
+}
+
+/* 
+//  js function which
+//  counts down how
+//  many days are left!
+//  ☆~~(>w0✿)b
 */
 
 function cntDwn() {
